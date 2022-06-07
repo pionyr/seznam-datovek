3e958064940eabd42ee54e5914b0c616457e6476
Ondřej Machulda <ondrej.machulda@gmail.com>
2022-06-07 15:09:42 +0200

    Remove API limit workaround

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-# Workaround for API providing only 100 records at once - do multiple specific queries (and filter duplicates later)
-INDEX=0
-for QUERY in "Pionýr, z. s." "pionýrská skupina" "krajská organizace Pionýra" "Pražská organizace Pionýra" "Zeměpisná společnost" "Florbalová akademie MB"; do
-  curl -s -o tmp-search-$INDEX.xml -d "<SearchSubjectRequest xmlns=\"http://seznam.gov.cz/ovm/ws/v1\"><Nazev>$QUERY</Nazev></SearchSubjectRequest>" -H 'Content-Type: application/xml' -X POST https://www.mojedatovaschranka.cz/sds/ws/call
-  ((++INDEX))
-done
+QUERY="Pionýr, z. s."
+SEARCH_RESULTS="tmp-search.xml"
+curl -s -o $SEARCH_RESULTS -d "<SearchSubjectRequest xmlns=\"http://seznam.gov.cz/ovm/ws/v1\"><Nazev>$QUERY</Nazev></SearchSubjectRequest>" -H 'Content-Type: application/xml' -X POST https://www.mojedatovaschranka.cz/sds/ws/call
 
-for SEARCH_RESULTS in tmp-search-*.xml; do
-  xmlstarlet sel -N n="http://seznam.gov.cz/ovm/ws/v1" -t -v "//n:NazevOsoby[starts-with(.,'Pion')]" <"$SEARCH_RESULTS" >>tmp-spolky.txt
-  xmlstarlet sel -N n="http://seznam.gov.cz/ovm/ws/v1" -t -v "//n:NazevOsoby[starts-with(.,'Pion')]/following-sibling::n:ISDS" <"$SEARCH_RESULTS" >>tmp-ids.txt
-  xmlstarlet sel -N n="http://seznam.gov.cz/ovm/ws/v1" -t -v "//n:NazevOsoby[starts-with(.,'Pion')]/../n:Ico" <$SEARCH_RESULTS >>tmp-ico.txt
+xmlstarlet sel -N n="http://seznam.gov.cz/ovm/ws/v1" -t -v "//n:NazevOsoby[starts-with(.,'Pion')]" <"$SEARCH_RESULTS" >>tmp-spolky.txt
+xmlstarlet sel -N n="http://seznam.gov.cz/ovm/ws/v1" -t -v "//n:NazevOsoby[starts-with(.,'Pion')]/following-sibling::n:ISDS" <"$SEARCH_RESULTS" >>tmp-ids.txt
+xmlstarlet sel -N n="http://seznam.gov.cz/ovm/ws/v1" -t -v "//n:NazevOsoby[starts-with(.,'Pion')]/../n:Ico" <$SEARCH_RESULTS >>tmp-ico.txt
 
-  # add newline at the end of each file
-  for FN in tmp-spolky.txt tmp-ids.txt tmp-ico.txt; do
-    echo "" >>$FN
-  done
+# add newline at the end of each file
+for FN in tmp-spolky.txt tmp-ids.txt tmp-ico.txt; do
+  echo "" >>$FN
 done
