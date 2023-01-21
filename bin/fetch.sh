5764778295b1cb35ba00b988c420c1ef3ffa6b48
Ondřej Machulda <ondrej.machulda@gmail.com>
2023-01-21 09:33:22 +0100

    Be more strict when filtering results

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -5,9 +5,9 @@ QUERY="Pionýr, z. s."
 SEARCH_RESULTS="tmp-search.xml"
 curl -s -o $SEARCH_RESULTS -d "<SearchSubjectRequest xmlns=\"http://seznam.gov.cz/ovm/ws/v1\"><Nazev>$QUERY</Nazev></SearchSubjectRequest>" -H 'Content-Type: application/xml' -X POST https://www.mojedatovaschranka.cz/sds/ws/call
 
-xmlstarlet sel -N n="http://seznam.gov.cz/ovm/ws/v1" -t -v "//n:NazevOsoby[starts-with(.,'Pion')]" <"$SEARCH_RESULTS" >>tmp-spolky.txt
-xmlstarlet sel -N n="http://seznam.gov.cz/ovm/ws/v1" -t -v "//n:NazevOsoby[starts-with(.,'Pion')]/following-sibling::n:ISDS" <"$SEARCH_RESULTS" >>tmp-ids.txt
-xmlstarlet sel -N n="http://seznam.gov.cz/ovm/ws/v1" -t -v "//n:NazevOsoby[starts-with(.,'Pion')]/../n:Ico" <$SEARCH_RESULTS >>tmp-ico.txt
+xmlstarlet sel -N n="http://seznam.gov.cz/ovm/ws/v1" -t -v "//n:NazevOsoby[starts-with(.,'Pionýr,')]" <"$SEARCH_RESULTS" >>tmp-spolky.txt
+xmlstarlet sel -N n="http://seznam.gov.cz/ovm/ws/v1" -t -v "//n:NazevOsoby[starts-with(.,'Pionýr,')]/following-sibling::n:ISDS" <"$SEARCH_RESULTS" >>tmp-ids.txt
+xmlstarlet sel -N n="http://seznam.gov.cz/ovm/ws/v1" -t -v "//n:NazevOsoby[starts-with(.,'Pionýr,')]/../n:Ico" <$SEARCH_RESULTS >>tmp-ico.txt
 
 # add newline at the end of each file
 for FN in tmp-spolky.txt tmp-ids.txt tmp-ico.txt; do
